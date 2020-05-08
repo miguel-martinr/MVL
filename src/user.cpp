a1b3357fc23c3129096a11953d499444a7f7b9a1
edgar-betis <57769905+edgar-betis@users.noreply.github.com>
2020-05-08 20:58:28 +0100

    Update user.cpp

--- src/user.cpp
+++ src/user.cpp
@@ -1,24 +1,3 @@
-/**********************************************************************
-* Project           : plataforma MVL (Moneda Virtual Local)
-*
-* Class             : user (usuario simple)
-*
-* Author            : Miguel Alejandro Martín Reyes (miguel-martinr)
-* e-mail            : alu0101209777@ull.edu.es
-* github            : https://github.com/miguel-martinr
-*
-* Date created      : 20200507
-*
-* References  :
-*
-*
-*
-* Revision History  :
-*
-* Date        Author              Ref    Revision
-* 20200507    miguel-martinr      1      ...
-*
-**********************************************************************/
 #include "../include/user.hpp"
 
 database user::dataBase_;
